a1bb3ead91b14ae93297695fe40e0996883ee28f
Jonatan Juárez <jonatan.ijp@gmail.com>
2014-07-27 08:54:30 -0500

    Refactor

--- parser.js
+++ parser.js
@@ -1,30 +1,51 @@
-var numbers = [
-  { ascii: [[" ", "_", " "],["|", " ", "|"],["|", "_", "|"]], amb: [8] },    //0
-  { ascii: [[" ", " ", " "],[" ", " ", "|"],[" ", " ", "|"]], amb: [7] },    //1
-  { ascii: [[" ", "_", " "],[" ", "_", "|"],["|", "_", " "]] },              //2
-  { ascii: [[" ", "_", " "],[" ", "_", "|"],[" ", "_", "|"]] },              //3
-  { ascii: [[" ", " ", " "],["|", "_", "|"],[" ", " ", "|"]] },              //4
-  { ascii: [[" ", "_", " "],["|", "_", " "],[" ", "_", "|"]], amb: [6, 9] }, //5
-  { ascii: [[" ", "_", " "],["|", "_", " "],["|", "_", "|"]], amb: [5] },    //6
-  { ascii: [[" ", "_", " "],[" ", " ", "|"],[" ", " ", "|"]], amb: [1] },    //7
-  { ascii: [[" ", "_", " "],["|", "_", "|"],["|", "_", "|"]], amb: [0, 9] }, //8
-  { ascii: [[" ", "_", " "],["|", "_", "|"],[" ", "_", "|"]], amb: [5, 8] }  //9
+var patterns = [
+  { ascii: [[" ", "_", " "],["|", " ", "|"],["|", "_", "|"]] }, //0
+  { ascii: [[" ", " ", " "],[" ", " ", "|"],[" ", " ", "|"]] }, //1
+  { ascii: [[" ", "_", " "],[" ", "_", "|"],["|", "_", " "]] }, //2
+  { ascii: [[" ", "_", " "],[" ", "_", "|"],[" ", "_", "|"]] }, //3
+  { ascii: [[" ", " ", " "],["|", "_", "|"],[" ", " ", "|"]] }, //4
+  { ascii: [[" ", "_", " "],["|", "_", " "],[" ", "_", "|"]] }, //5
+  { ascii: [[" ", "_", " "],["|", "_", " "],["|", "_", "|"]] }, //6
+  { ascii: [[" ", "_", " "],[" ", " ", "|"],[" ", " ", "|"]] }, //7
+  { ascii: [[" ", "_", " "],["|", "_", "|"],["|", "_", "|"]] }, //8
+  { ascii: [[" ", "_", " "],["|", "_", "|"],[" ", "_", "|"]] }  //9
 ];
 
 module.exports = {
   process: function(accounts, cb){
-    for(i = 0; i < accounts.length; i++){
-      for(y = 0; y < accounts[i].numbers.length; y++){
-        for(x = 0; x < numbers.length; x++){
-          if(JSON.stringify(numbers[x].ascii) === JSON.stringify(accounts[i].numbers[y].ascii)){
-            accounts[i].numbers[y].output = x;
-            x = numbers.length;
-          }
-        }
-      }
+    accounts.forEach(function(account){
+      account.numbers.forEach(function(number){
+        this.identify(number);
+        console.log(number);
+      }.bind(this));
 
-      cb(accounts[i].numbers, this.calculateChecksum(accounts[i]));
-    }
+      cb(account.numbers, this.calculateChecksum(account));
+    }.bind(this));
+
+    //for(i = 0; i < accounts.length; i++){
+      //for(y = 0; y < accounts[i].numbers.length; y++){
+        //for(x = 0; x < numbers.length; x++){
+          //if(JSON.stringify(numbers[x].ascii) === JSON.stringify(accounts[i].numbers[y].ascii)){
+            //accounts[i].numbers[y].output = x;
+            //x = numbers.length;
+          //}
+        //}
+      //}
+
+      //cb(accounts[i].numbers, this.calculateChecksum(accounts[i]));
+    //}
+  },
+
+  identify: function(number){
+    patterns.forEach(function(pattern, index){
+      if(JSON.stringify(pattern.ascii) === JSON.stringify(number.ascii)){
+        number.output = index;
+        console.log("INDEX", index);
+        return true;
+      }
+      console.log("HOLA", index);
+    });
+    number.output = "?";
   },
 
   calculateChecksum: function(account){
